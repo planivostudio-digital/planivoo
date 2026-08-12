7f6ab763e248e23ed9147dcaaed985aa502c3cca
planivostudio-digital <planivostudio@gmail.com>
2026-08-12 17:36:32 +0500

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,27 @@
+import Hero from "../sections/Hero";
+import Philosophy from "../sections/Philosophy";
+import CurrentProduct from "../sections/CurrentProduct";
+import Ecosystem from "../sections/Ecosystem";
+import Discovery from "../sections/Discovery";
+import WhyPlanivo from "../sections/WhyPlanivo";
+import Storytelling from "../sections/Storytelling";
+import About from "../sections/About";
+import Journal from "../sections/Journal";
+import FinalCTA from "../sections/FinalCTA";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Philosophy />
+      <CurrentProduct />
+      <Ecosystem />
+      <Discovery />
+      <WhyPlanivo />
+      <Storytelling />
+      <About />
+      <Journal />
+      <FinalCTA />
+    </>
+  );
+}
